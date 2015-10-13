265b4ed33b9de18b4373c0abb851361710a14932
gaerfield <gaerfield@posteo.net>
2015-10-13 16:13:05 +0200

    case-insensitive highlighting of search-result

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
+function preg_quote(str, delimiter) {
+    //  discuss at: http://phpjs.org/functions/preg_quote/
+    // original by: booeyOH
+    // improved by: Ates Goral (http://magnetiq.com)
+    // improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // improved by: Brett Zamir (http://brett-zamir.me)
+    // bugfixed by: Onno Marsman
+    //   example 1: preg_quote("$40");
+    //   returns 1: '\\$40'
+    //   example 2: preg_quote("*RRRING* Hello?");
+    //   returns 2: '\\*RRRING\\* Hello\\?'
+    //   example 3: preg_quote("\\.+*?[^]$(){}=!<>|:");
+    //   returns 3: '\\\\\\.\\+\\*\\?\\[\\^\\]\\$\\(\\)\\{\\}\\=\\!\\<\\>\\|\\:'
+
+    return String(str)
+        .replace(new RegExp('[.\\\\+*?\\[\\^\\]$(){}=!<>|:\\' + (delimiter || '') + '-]', 'g'), '\\$&');
+}
+
 // app.js
 angular.module('sortApp', ['ngSanitize'])
 
     .filter('highlight', function ($sce) {
         return function (text, phrase) {
-            if (phrase) text = text.replace(phrase,
-                '<span class="highlighted">' + phrase + '</span>')
+            if (phrase && text.toLowerCase().contains(phrase))
+                text = text.replace( new RegExp( "(" + preg_quote( phrase ) + ")" , 'gi' ), '<span class="highlighted">$1</span>' );
+            
             return $sce.trustAsHtml(text)
         }
     })
